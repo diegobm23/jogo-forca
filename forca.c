bf0a8cb659d08117fe8a38692d9be8fcd70479fd
diegobm23 <diego@asaplog.com.br>
2022-01-24 14:20:27 -0300

    Melhoria de código separando em funções; criação de header file; criação de um arquivo de palavras; desenvolvido função perder; desenvolvido função de ganhar; desenvolvido leitura do arquivo de palavras para escolha de uma aleatória;

--- forca.c
+++ forca.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "forca.h"
 
 char palavrasecreta[20];
 char chutes[26];
-int tentativas = 0;
+int chutesdados = 0;
 
 void abertura() {
   printf("\n/****************/\n");
@@ -16,24 +19,12 @@ void chuta() {
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    chutes[tentativas] = chute;
-}
-
-int jachutou(char letra) {
-  int achou = 0;
-
-  for (int j = 0; j < tentativas; j++) {
-    if (chutes[j] == letra) {
-      achou = 1;
-      break;
-    }
-  }
-
-  return achou;
+    chutes[chutesdados] = chute;
+    chutesdados++;
 }
 
 void desenhaforca() {
-    printf("Você já deu %d chutes\n", tentativas);
+    printf("Você já deu %d chutes\n", chutesdados);
 
     for (int i = 0; i < strlen(palavrasecreta); i++) {
 
@@ -49,22 +40,77 @@ void desenhaforca() {
 }
 
 void escolhepalavra() {
-    sprintf(palavrasecreta, "MELANCIA");
+  FILE* file;
+  file = fopen("palavras.txt", "r");
+
+  if (file == 0) {
+    printf("OPS, Arquivo de palavras não encontrado =(");
+    exit(1);
+  }
+
+  int qtdpalavras;
+  fscanf(file, "%d", &qtdpalavras);
+
+
+  srand(time(0));
+  int randomico = rand() % qtdpalavras;
+
+  for (int i = 0; i < randomico; i++) {
+    fscanf(file, "%s", palavrasecreta);
+  }
+
+  fclose(file);
+}
+
+int enforcou() {
+  int erros = 0;
+
+  for (int i = 0; i < chutesdados; i++) {
+    int existe = 0;
+
+    for (int j = 0; j < strlen(palavrasecreta); j++) {
+      if (chutes[i] == palavrasecreta[j]) {
+        existe = 1;
+        break;
+      }
+    }
+
+    if (!existe) erros++;
+  }
+
+  return erros >= 5;
+}
+
+int acertou() {
+  for (int i = 0; i < strlen(palavrasecreta); i++) {
+    if (!jachutou(palavrasecreta[i])) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int jachutou(char letra) {
+  int achou = 0;
+
+  for (int j = 0; j < chutesdados; j++) {
+    if (chutes[j] == letra) {
+      achou = 1;
+      break;
+    }
+  }
+
+  return achou;
 }
 
 int main() {
-  int acertou = 0;
-  int enforcou = 0;
-
   abertura();
   escolhepalavra();
 
   do {
     desenhaforca();
     chuta();
-
-    tentativas++;
-
-  } while (!acertou && !enforcou);
+  } while (!acertou() && !enforcou());
 
 }
